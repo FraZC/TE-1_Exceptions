4ac9d40dc3cbf7a8ee26a994ebe4c729f5e85f7a
Francisco <franzc@estudiantec.cr>
2021-03-24 16:27:35 -0600

    Adding my own exceptions

---
+++ TE1_Francisco Zamora Corrales/src/My_own_Java_exception/First_one/JustNaturalNumberException.java
@@ -0,0 +1,7 @@
+package My_own_Java_exception.First_one;
+//Exception that only allows natural numbers
+public class JustNaturalNumberException extends Exception{
+    public JustNaturalNumberException (String message){
+        super (message);
+    }
+}
